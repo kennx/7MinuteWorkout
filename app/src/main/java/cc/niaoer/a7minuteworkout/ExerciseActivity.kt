43fc4ed9c3caabbe5b5cdb6b328d700b817a8454
Kenn Zh <kenn@Kenns-Mac-mini.local>
2023-02-19 16:28:44 +0800

    update

--- app/src/main/java/cc/niaoer/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/cc/niaoer/a7minuteworkout/ExerciseActivity.kt
@@ -3,12 +3,24 @@ package cc.niaoer.a7minuteworkout
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import cc.niaoer.a7minuteworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private var restTimer: CountDownTimer? = null
+    private var restProgress: Int = 0
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress: Int = 0
+    private var exerciseTimerDuration: Long = 30
+
+    private var exerciseList: ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -20,5 +32,86 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbarExerciseActivity.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        setupRestView()
+
+        exerciseList = Constants.defaultExerciseList()
+
+    }
+
+    override fun onDestroy() {
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+    }
+
+    private fun setRestProgressBar() {
+        binding.progressBar.progress = restProgress
+        restTimer = object: CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding.progressBar.progress = 10 - restProgress
+                binding.tvTimer.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                currentExercisePosition++
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setExerciseProgressBar() {
+        binding.progressBarExercise.progress = exerciseProgress
+        exerciseTimer = object: CountDownTimer(exerciseTimerDuration * 1000, 1000) {
+            override fun onTick(p0: Long) {
+                exerciseProgress++
+                binding.progressBarExercise.progress = exerciseTimerDuration.toInt() - exerciseProgress
+                binding.tvExerciseTimer.text = (exerciseTimerDuration.toInt() - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                if (currentExercisePosition < exerciseList?.size!! - 1) {
+                    setupRestView()
+                } else {
+                    Toast.makeText(
+                        this@ExerciseActivity,
+                        "Congratulations! You have completed the 7 minutes workout.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }.start()
+    }
+
+    private fun setupExerciseView() {
+        binding.llRestView.visibility = View.GONE
+        binding.llExerciseView.visibility = View.VISIBLE
+
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+
+        setExerciseProgressBar()
+
+        binding.ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        binding.tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
+    }
+
+    private fun setupRestView() {
+
+        binding.llRestView.visibility = View.VISIBLE
+        binding.llExerciseView.visibility = View.GONE
+
+
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
     }
 }
